e5130e1c35f45aed5f9cf3487b733c004405884f
anoopgupta112 <anoopgupta112>
2022-04-07 23:59:51 +0530

    Checkpoint 2

---
+++ src/common/header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Header.css'
+class Header extends Component {
+    render() {
+        return (
+            <div><img src='https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg' alt='error' className='img-file'></img></div>
+        )
+    }
+}
+export default Header;
